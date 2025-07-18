9e4f5648d184986ee8d9c588d10a97250f903ce4
unblockinstagram <irinalarikova304@gmail.com>
2025-07-18 13:51:47 +0300

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UnblockInstagramIndia from "./UnblockInstagramIndia.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UnblockInstagramIndia />
+  </React.StrictMode>
+);
